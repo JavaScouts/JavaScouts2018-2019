134bb2eaae76555a413fbf30c5eb3f357a68d053
Linus\Leon <javascouts@gmail.com>
2018-09-17 19:08:23 -0400

    ignore

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TelemetryDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TelemetryDrive.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 /**
@@ -12,28 +13,33 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 public class TelemetryDrive extends LinearOpMode {
 
     RobotHardware robot = new RobotHardware();
-
+    DcMotor mineralarm;
     @Override
     public void runOpMode() {
 
-        robot.init(hardwareMap, this);
+//        robot.init(hardwareMap, this);
+
+        mineralarm = hardwareMap.dcMotor.get("m");
+/*
 
         robot.rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
         robot.backRDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+*/
 
         waitForStart();
 
         while(opModeIsActive()) {
+/*
 
             robot.manualDrive();
             robot.moveRobot();
+*/
+
+            mineralarm.setPower(gamepad2.right_stick_y);
 
             telemetry.addLine();
 
             telemetry.update();
-
-
-
 
         }
 
