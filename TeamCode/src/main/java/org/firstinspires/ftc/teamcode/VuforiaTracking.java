b9cee3b6095106ab21df00dced026fb32212df30
Linus\Leon <javascouts@gmail.com>
2018-11-05 19:54:06 -0500

    try some new stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaTracking.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaTracking.java
@@ -1,35 +1,20 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.graphics.YuvImage;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-<<<<<<< HEAD
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.vuforia.Image;
-import com.vuforia.Vuforia;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
-import org.opencv.core.MatOfPoint;
-import org.opencv.core.Rect;
-import org.opencv.imgproc.Imgproc;
 
 import java.util.List;
-import java.util.Locale;
 
-import static com.sun.tools.javac.util.Constants.format;
-=======
-import com.qualcomm.robotcore.hardware.DcMotor;
-
-import org.firstinspires.ftc.robotcore.external.ClassFactory;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
-
-import java.util.List;
->>>>>>> 1b45de96f9c91a82095f03347097adc292295373
+import org.firstinspires.ftc.robotcore.internal.tfod.VuforiaFrameGenerator;
 
 /**
  * Created by Liam on 9/13/2018.
@@ -37,19 +22,14 @@ import java.util.List;
 @TeleOp(name="VuforiaTracking")
 public class VuforiaTracking extends LinearOpMode {
 
-<<<<<<< HEAD
-    //RobotHardware robot = new RobotHardware();
-    VuforiaOpenCVBridge vuforia;
-    float centerX;
-=======
     private static final String TFOD_MODEL_ASSET = "RoverRuckus.tflite";
     private static final String LABEL_GOLD_MINERAL = "Gold Mineral";
     private static final String LABEL_SILVER_MINERAL = "Silver Mineral";
     private VuforiaLocalizer vuforia;
     private TFObjectDetector tfod;
+    private VuforiaFrameGenerator vfg;
     RobotHardware robot = new RobotHardware();
 
->>>>>>> 1b45de96f9c91a82095f03347097adc292295373
     WebcamName webcamName;
 
     @Override
@@ -57,70 +37,19 @@ public class VuforiaTracking extends LinearOpMode {
 
         /*robot.init(hardwareMap, this);
 
-<<<<<<< HEAD
         robot.rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
         robot.backRDrive.setDirection(DcMotorSimple.Direction.REVERSE);
 */
-        webcamName = hardwareMap.get(WebcamName.class, "w1");
-=======
-        robot.rightDrive.setDirection(DcMotor.Direction.REVERSE);
-        robot.backRDrive.setDirection(DcMotor.Direction.REVERSE);
-        */
-        webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
->>>>>>> 1b45de96f9c91a82095f03347097adc292295373
+        //initVuforia()
 
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
-
-        parameters.vuforiaLicenseKey = "AVoYG9X/////AAABmTTi1I64gUR7oUdT0QMIZVw7tlyP8f2ssEm01N2IGE6F3ikzxOypSwQXtCSPhv8PqIRohipOk2GI+pn/+QAtwZtTPVoCId9/w9wuDgGqsO/g8HbJ9pwGfO6Rb85AM2BJa5ogWpCbXerFtp9Qb2lhvfUW50pWEFbA54SI6lbyePynx3844TgIpEhQ7jHLElcxchcrX0is8VCfhjHjV53sUsFSanRL86B7tALf8ULPz2e0hISvqV9/IoJVkeIWsRzoISGMooLydCsXUJF2QQc095ktlp0sRcekZoNcNp3+wIFzP2Em6Bce5WBvpeciVuBDy3ebgK5a3WqPB2yTmkSFuu24BGI3XbEmNVFzuQdH3//r";
-
-        parameters.cameraName = webcamName;
-
-<<<<<<< HEAD
-        vuforia = new VuforiaOpenCVBridge(parameters);
-        vuforia.start();
-=======
-        vuforia = ClassFactory.getInstance().createVuforia(parameters);
-
-
-
-        if(ClassFactory.getInstance().canCreateTFObjectDetector()) {
-            int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
-                    "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-            TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
-            tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
-            tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_GOLD_MINERAL, LABEL_SILVER_MINERAL);
-        } else {
-            telemetry.addData("Sorry!", "This device is not compatible with TFOD");
-        }
-
-
-
-        telemetry.addData("Initialization", "Complete.");
-        telemetry.update();
-
-
->>>>>>> 1b45de96f9c91a82095f03347097adc292295373
+        vfg = new VuforiaFrameGenerator(vuforia, 0);
 
         waitForStart();
-
-
-<<<<<<< HEAD
-            List<MatOfPoint> contours = vuforia.getContours();
-            for (int i = 0; i < contours.size(); i++) {
-                // get the bounding rectangle of a single contour, we use it to get the x/y center
-                // yes there's a mass center using Imgproc.moments but w/e
-                Rect boundingRect = Imgproc.boundingRect(contours.get(i));
-                centerX = (boundingRect.x + boundingRect.width) / 2;
-                telemetry.addData("contour" + Integer.toString(i),
-                        String.format(Locale.getDefault(), "(%d, %d)", (boundingRect.x + boundingRect.width) / 2, (boundingRect.y + boundingRect.height) / 2));
-=======
 
         if(opModeIsActive()) {
 
             if(tfod != null) {
                 tfod.activate();
->>>>>>> 1b45de96f9c91a82095f03347097adc292295373
             }
 
             while (opModeIsActive()) {
@@ -130,8 +59,6 @@ public class VuforiaTracking extends LinearOpMode {
                     // the last time that call was made.
                     List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
 
-<<<<<<< HEAD
-=======
                     if (updatedRecognitions != null) {
 
                         telemetry.addData("# Object Detected", updatedRecognitions.size());
@@ -188,5 +115,31 @@ public class VuforiaTracking extends LinearOpMode {
 
     }
 
->>>>>>> 1b45de96f9c91a82095f03347097adc292295373
+    void initVuforia() {
+
+        webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        parameters.vuforiaLicenseKey = "AVoYG9X/////AAABmTTi1I64gUR7oUdT0QMIZVw7tlyP8f2ssEm01N2IGE6F3ikzxOypSwQXtCSPhv8PqIRohipOk2GI+pn/+QAtwZtTPVoCId9/w9wuDgGqsO/g8HbJ9pwGfO6Rb85AM2BJa5ogWpCbXerFtp9Qb2lhvfUW50pWEFbA54SI6lbyePynx3844TgIpEhQ7jHLElcxchcrX0is8VCfhjHjV53sUsFSanRL86B7tALf8ULPz2e0hISvqV9/IoJVkeIWsRzoISGMooLydCsXUJF2QQc095ktlp0sRcekZoNcNp3+wIFzP2Em6Bce5WBvpeciVuBDy3ebgK5a3WqPB2yTmkSFuu24BGI3XbEmNVFzuQdH3//r";
+
+        parameters.cameraName = webcamName;
+
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        if(ClassFactory.getInstance().canCreateTFObjectDetector()) {
+            int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                    "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+            TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+            tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+            tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_GOLD_MINERAL, LABEL_SILVER_MINERAL);
+        } else {
+            telemetry.addData("Sorry!", "This device is not compatible with TFOD");
+        }
+
+        telemetry.addData("Initialization", "Complete.");
+        telemetry.update();
+    }
+
 }
